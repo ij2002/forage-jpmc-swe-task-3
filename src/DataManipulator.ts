8042e030d540f8a166a8cb12230d894850acbd8f
ij2002 <ij2002@hw.ac.uk>
2024-07-15 06:21:51 +0530

    completed

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,36 @@
+// responsible for processing the raw stock data we receive from the server before the Graph component renders it
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  // stock: string,
+  // top_ask_price: number,
+  // top_bid_price: number, 
   timestamp: Date,
+  ratio: number, 
+  upper_bound: number, 
+  lower_bound: number, 
+  trigger_alert: number | undefined, 
+  price_abc: number, 
+  price_def: number, 
 }
 
-
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + 0.04; // 4% above the average ratio
+    const lowerBound = 1 - 0.04; // 4% below the average ratio
+
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? 
+                 serverResponds[0].timestamp : serverResponds[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+    };
   }
 }
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -23,10 +23,17 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+      // stock: 'string',
+      // top_ask_price: 'float',
+      // top_bid_price: 'float',
+      timestamp: 'date', //tracking all w.r.t time, need timestamp field
+      ratio: 'float', //added to track ratio of two stocks
+      upper_bound: 'float', //added to track upper bound
+      lower_bound: 'float', //added to track lower bound
+      // trigger_alert: 'boolean', //added to track when bounds are crossed
+      trigger_alert: 'float', //added to track when bounds are crossed
+      price_abc: 'float', //needed to calculate ratio
+      price_def: 'float', //needed to calculate ratio
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -35,24 +42,27 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('view', 'y_line'); //type of graph to visualize data
+      // elem.setAttribute('column-pivots', '["stock"]'); //removed bc we track ratios, not separate prices
+      elem.setAttribute('row-pivots', '["timestamp"]'); //maps each datapoint based on timestamp
+      elem.setAttribute('columns', '["ratio", "upper_bound", "lower_bound", "trigger_alert"]'); //focus on specific data parts to avoid noise
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
+        timestamp: 'distinct count', //consolidate data points by timestamp
+        ratio: 'avg', //avg values of non-unique fields
+        upper_bound: 'avg', //avg values of non-unique fields
+        lower_bound: 'avg', //avg values of non-unique fields
+        trigger_alert: 'avg', //avg data points by timestamp
+        price_abc: 'avg', //avg values of non-unique fields
+        price_def: 'avg', //avg values of non-unique fields
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+      this.table.update([
+        DataManipulator.generateRow(this.props.data), //update table with new data
+      ] as unknown as TableData); //assertion to ensure the data is treated as TableData
     }
   }
 }
